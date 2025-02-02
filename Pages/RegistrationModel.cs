cdd87b1bcf53b3e81ce0cce65b4e9b6a3fd48642
Martin <Martin@testmail.com>
2025-02-02 20:59:34 +0100

    uploading the App

---
+++ Pages/RegistrationModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApp.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
